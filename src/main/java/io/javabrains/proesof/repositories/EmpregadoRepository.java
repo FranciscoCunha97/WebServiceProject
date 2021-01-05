a8f93b7c5c8a40f33af04fc1cf107d149239d726
Vasco Cardoso <35314@ufp.edu.pt>
2021-01-05 21:58:51 +0000

    Vasco

--- src/main/java/io/javabrains/proesof/repositories/EmpregadoRepository.java
+++ src/main/java/io/javabrains/proesof/repositories/EmpregadoRepository.java
@@ -13,8 +13,8 @@ import java.util.Optional;
 @Repository
 public interface EmpregadoRepository extends CrudRepository<Empregado, Long>
 {
-  //  @Query("SELECT e FROM Empregado e join e.tarefas t  where (:cargo is null or e.cargo=:cargo)")
-   // List<Empregado> pesquisaEmpregados(@Param("tarefa") Tarefa tarefa, @Param("cargo")Cargo cargo);
+    @Query("SELECT e FROM Empregado e join e.tarefas t  where (:cargo is null or e.cargo=:cargo)")
+    List<Empregado> pesquisaEmpregados(@Param("tarefa") Tarefa tarefa, @Param("cargo")Cargo cargo);
 
 
     Optional<Empregado> findByEmail(String email);
