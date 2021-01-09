9267f61ea6dfc35ac23fbf586ba964b993f367a0
Vasco Cardoso <35314@ufp.edu.pt>
2021-01-09 15:49:47 +0000

    teste em falta

--- src/test/java/io/javabrains/proesof/models/ClienteTest.java
+++ src/test/java/io/javabrains/proesof/models/ClienteTest.java
@@ -2,11 +2,23 @@ package io.javabrains.proesof.models;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ClienteTest {
 
     @Test
     void addProjeto() {
+        Cliente cliente = new Cliente();
+        Projeto projeto = new Projeto();
+        projeto.setNome("Aplicacao Web");
+        projeto.setDataInicio(LocalDate.now());
+
+        assertEquals(0,cliente.getProjetos().size());
+        cliente.addProjeto(projeto);
+        assertEquals(1, cliente.getProjetos().size());
+        cliente.addProjeto(projeto);
+        assertEquals(1, cliente.getProjetos().size());
     }
 }
