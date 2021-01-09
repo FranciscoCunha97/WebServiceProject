d2a0a512370f5fb969934f6af4af81d8b16c891d
Francisco Cunha <francisco.97@live.com.pt>
2021-01-09 17:37:13 +0000

    get do projeto

--- src/main/java/io/javabrains/proesof/controllers/ProjetoController.java
+++ src/main/java/io/javabrains/proesof/controllers/ProjetoController.java
@@ -1,12 +1,14 @@
 package io.javabrains.proesof.controllers;
 
 
+import io.javabrains.proesof.dtos.EmpregadoResponseDTO;
 import io.javabrains.proesof.dtos.ProjetoCreateDTO;
 import io.javabrains.proesof.dtos.ProjetoResponseDTO;
 import io.javabrains.proesof.dtos.TarefaCreateDTO;
 import io.javabrains.proesof.dtos.conversores.ConverterProjetoParaDTO;
 import io.javabrains.proesof.models.Projeto;
 import io.javabrains.proesof.services.ProjetoService;
+import io.swagger.models.properties.ObjectProperty;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -34,6 +36,17 @@ public class ProjetoController {
     public ResponseEntity<ProjetoResponseDTO> adicionaTarefa(@PathVariable Long projetoId, @RequestBody TarefaCreateDTO tarefa){
         Optional<Projeto> optionalProjeto = projetoService.createTarefaAoProjeto(projetoId,tarefa.converter());
         return optionalProjeto.map(projeto -> ResponseEntity.ok(converterProjetoParaDTO.converter(projeto))).orElseGet(() -> ResponseEntity.badRequest().build());
+    }
+
+    @GetMapping("/{idProjeto}/valor")
+    public ResponseEntity<ProjetoResponseDTO> getValorByProjetoId(@PathVariable Long projetoId){
+        Optional<Projeto> optionalProjeto = projetoService.findById(projetoId);
+        //Projeto projeto = new Projeto();
+        //int valorTotal = projeto.valorTotal();
+        return optionalProjeto.map(projeto -> {
+            ProjetoResponseDTO projetoResponseDTO = converterProjetoParaDTO.converter(projet);
+            return ResponseEntity.ok(projetoResponseDTO);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
 }
